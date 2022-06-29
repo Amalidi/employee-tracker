7b4d46e4e3c76801ed3c332d3e1b521b185bcfb6
Amalidi <A.idi12@outlook.com>
2022-06-29 19:22:49 +0100

    update

--- src/schema/schema.sql
+++ src/schema/schema.sql
@@ -16,7 +16,6 @@ CREATE TABLE role (
   salary DECIMAL(8,2),
   department_id INT NOT NULL,
   PRIMARY KEY (id),
-  CONSTRAINT fk_department
   FOREIGN KEY (department_id) REFERENCES department(id)
   ON DELETE CASCADE
 );
@@ -28,9 +27,9 @@ CREATE TABLE employee (
   role_id INT NOT NULL,
   manager_id INT,
   PRIMARY KEY (id),
-  CONSTRAINT fk_role FOREIGN KEY (role_id)
+  FOREIGN KEY (role_id)
   REFERENCES role (id) ON DELETE CASCADE,
-  CONSTRAINT fk_employee FOREIGN KEY (manager_id)
+  FOREIGN KEY (manager_id)
   REFERENCES employee (id) 
   ON DELETE SET NULL
 );
